afd6970fe927eb8d3046691f9f4973126462507a
Akshay Urankar <101685739+akshayurankar48@users.noreply.github.com>
2026-02-28 10:24:50 +0530

    feat: adapt boilerplate to WP Agent scaffold
    - Rename wp-plugin-base.php to wp-agent.php with updated plugin header
    - Change namespace from WPB to WPAgent, constants from WPB_ to WP_AGENT_
    - Update text domain from wp-plugin-base to wp-agent across all configs
    - Add register_activation_hook and register_deactivation_hook
    - Add uninstall.php for clean data removal
    - Add WP_AGENT_DB_VER constant for future migration tracking
    - Update phpcs.xml, phpstan.neon, .eslintrc.js, Gruntfile.js, composer.json
    - Rename test stubs from wpb-constants to wp-agent-constants
    - Add ABSPATH guards to all PHP files
    - Add WP-Agent-PR.md implementation plan with finalized design decisions

--- plugin-loader.php
+++ plugin-loader.php
@@ -2,16 +2,18 @@
 /**
  * Plugin Loader.
  *
- * @package {{package}}
- * @since {{since}}
+ * @package WPAgent
+ * @since 1.0.0
  */
 
-namespace WPB;
+defined( 'ABSPATH' ) || exit;
+
+namespace WPAgent;
 
 /**
  * Plugin_Loader
  *
- * @since X.X.X
+ * @since 1.0.0
  */
 class Plugin_Loader {
 
@@ -19,19 +21,19 @@ class Plugin_Loader {
 	 * Instance
 	 *
 	 * @access private
-	 * @var object Class Instance.
-	 * @since X.X.X
+	 * @var Plugin_Loader|null Class Instance.
+	 * @since 1.0.0
 	 */
-	private static $instance;
+	private static $instance = null;
 
 	/**
 	 * Initiator
 	 *
-	 * @since X.X.X
-	 * @return object initialized object of class.
+	 * @since 1.0.0
+	 * @return Plugin_Loader Initialized object of class.
 	 */
 	public static function get_instance() {
-		if ( ! isset( self::$instance ) ) {
+		if ( null === self::$instance ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
@@ -40,7 +42,8 @@ class Plugin_Loader {
 	/**
 	 * Autoload classes.
 	 *
-	 * @param string $class class name.
+	 * @param string $class Class name.
+	 * @since 1.0.0
 	 */
 	public function autoload( $class ) {
 		if ( 0 !== strpos( $class, __NAMESPACE__ ) ) {
@@ -57,9 +60,9 @@ class Plugin_Loader {
 			)
 		);
 
-		$file = WPB_DIR . $filename . '.php';
+		$file = WP_AGENT_DIR . $filename . '.php';
 
-		// if the file redable, include it.
+		// If the file is readable, include it.
 		if ( is_readable( $file ) ) {
 			require_once $file;
 		}
@@ -68,10 +71,9 @@ class Plugin_Loader {
 	/**
 	 * Constructor
 	 *
-	 * @since X.X.X
+	 * @since 1.0.0
 	 */
 	public function __construct() {
-
 		spl_autoload_register( [ $this, 'autoload' ] );
 
 		add_action( 'plugins_loaded', [ $this, 'load_textdomain' ] );
@@ -79,23 +81,24 @@ class Plugin_Loader {
 
 	/**
 	 * Load Plugin Text Domain.
-	 * This will load the translation textdomain depending on the file priorities.
-	 *      1. Global Languages /wp-content/languages/wp-plugin-base/ folder
-	 *      2. Local dorectory /wp-content/plugins/wp-plugin-base/languages/ folder
 	 *
-	 * @since X.X.X
+	 * This will load the translation textdomain depending on the file priorities.
+	 *      1. Global Languages /wp-content/languages/wp-agent/ folder
+	 *      2. Local directory /wp-content/plugins/wp-agent/languages/ folder
+	 *
+	 * @since 1.0.0
 	 * @return void
 	 */
 	public function load_textdomain() {
 		// Default languages directory.
-		$lang_dir = WPB_DIR . 'languages/';
+		$lang_dir = WP_AGENT_DIR . 'languages/';
 
 		/**
 		 * Filters the languages directory path to use for plugin.
 		 *
 		 * @param string $lang_dir The languages directory path.
 		 */
-		$lang_dir = apply_filters( 'wpb_languages_directory', $lang_dir );
+		$lang_dir = apply_filters( 'wp_agent_languages_directory', $lang_dir );
 
 		// Traditional WordPress plugin locale filter.
 		global $wp_version;
@@ -107,33 +110,33 @@ class Plugin_Loader {
 		}
 
 		/**
-		 * Language Locale for plugin
+		 * Language Locale for plugin.
 		 *
-		 * @var $get_locale The locale to use.
-		 * Uses get_user_locale()` in WordPress 4.7 or greater,
-		 * otherwise uses `get_locale()`.
+		 * @var string $get_locale The locale to use.
+		 * Uses get_user_locale() in WordPress 4.7 or greater,
+		 * otherwise uses get_locale().
 		 */
-		$locale = apply_filters( 'plugin_locale', $get_locale, 'wp-plugin-base' );
-		$mofile = sprintf( '%1$s-%2$s.mo', 'wp-plugin-base', $locale );
+		$locale = apply_filters( 'plugin_locale', $get_locale, 'wp-agent' );
+		$mofile = sprintf( '%1$s-%2$s.mo', 'wp-agent', $locale );
 
 		// Setup paths to current locale file.
 		$mofile_global = WP_LANG_DIR . '/plugins/' . $mofile;
 		$mofile_local  = $lang_dir . $mofile;
 
 		if ( file_exists( $mofile_global ) ) {
-			// Look in global /wp-content/languages/wp-plugin-base/ folder.
-			load_textdomain( 'wp-plugin-base', $mofile_global );
+			// Look in global /wp-content/languages/wp-agent/ folder.
+			load_textdomain( 'wp-agent', $mofile_global );
 		} elseif ( file_exists( $mofile_local ) ) {
-			// Look in local /wp-content/plugins/wp-plugin-base/languages/ folder.
-			load_textdomain( 'wp-plugin-base', $mofile_local );
+			// Look in local /wp-content/plugins/wp-agent/languages/ folder.
+			load_textdomain( 'wp-agent', $mofile_local );
 		} else {
 			// Load the default language files.
-			load_plugin_textdomain( 'wp-plugin-base', false, $lang_dir );
+			load_plugin_textdomain( 'wp-agent', false, $lang_dir );
 		}
 	}
 }
 
 /**
- * Kicking this off by calling 'get_instance()' method
+ * Kicking this off by calling 'get_instance()' method.
  */
 Plugin_Loader::get_instance();
